699b254fbf491b32a09ceaf2b1fefb44ec6fb05f
ShrikiQA <shriki1019@gmail.com>
2026-03-24 20:03:51 +0530

    day15 - part-2

--- day-15/day15.js
+++ day-15/day15.js
@@ -181,20 +181,158 @@ arr1 = [11, 21, 45];
 arr1.length = 7;
 console.log(arr1);
 
-// Reverse Method
+// Concat Method
 
-const names = ['Tom', 'Alex', 'Bob', 'John'];
+const first1 = [1,2,3];
+const Second1 = [4,5,6];
+const thrid1 = [7,8,9];
 
-console.log(names.reverse());
+console.log(first1.concat(Second1)); //  [1, 2, 3, 4, 5, 6]
+console.log(first1.concat(Second1, thrid1)); //  [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-console.log(names);
+// Join Method
+
+const emotions1 = ['happy', 'sad', 'smile', 'cry'];
+
+console.log(emotions1.join()); // happy,sad,smile,cry
+
+console.log(emotions1.join("<=>")); // happy<=>sad<=>smile<=>cry
+
+// Fill Method 
+//this method is used for Overide the elements
+
+const colours = ['red','blue','green'];
+
+colours.fill("pink");
+
+console.log(colours); // ['pink', 'pink', 'pink']
+
+const colours1 = ['red','blue','green'];
+
+colours1.fill("Pink",1,3);
+console.log(colours1); // ['red', 'Pink', 'Pink']
+
+const colours2 = ['red','blue','green'];
+
+colours2.fill("Pink",1,2);
+console.log(colours2); // ['red', 'Pink', 'green']
+
+// Inculdes Method
+// This is used for element is present or not in array
+
+const names2 = ['Tom', 'Jack', 'Cook'];
+
+console.log(names2.includes('Tom')); // True
+console.log(names2.includes('tom')); // False
+console.log(names2.includes('Test')); // False
+console.log(names2.includes(12)); // False
 
 // Index of ()
 
 const names1 = [15, 13, 1, 1000, 12, 99];
 
-console.log("Sort Order", names1.sort());
+console.log(names1.indexOf(13)); // 1
+console.log(names1.indexOf(1000)); // 3
+console.log(names1.indexOf(15)); // 0
 
+// Reverse Method
 
+const names = ['Tom', 'Alex', 'Bob', 'John'];
 
+console.log(names.reverse()); // ['John', 'Bob', 'Alex', 'Tom']
 
+console.log(names); // ['John', 'Bob', 'Alex', 'Tom']
+
+// Sort Method
+
+const artist = ['John', 'Leo','Charles','Anna', 'Barent'];
+console.log(artist.sort()); // ['Anna', 'Barent', 'Charles', 'John', 'Leo']
+
+// For Descending Order
+
+console.log(artist.sort(function(a, b){
+    return a ===b ?0: a>b?-1:1;
+})); // ['Leo', 'John', 'Charles', 'Barent', 'Anna']
+
+// For Numbers sort method is not work which is why we have to use above method
+
+const numbers = [15, 13, 1, 1000, 12, 99] 
+
+console.log(numbers.sort());//  [1, 1000, 12, 13, 15, 99]
+
+console.log(numbers.sort(function(a, b){
+    return a ===b ?0: a>b?-1:1;
+})); // [1000, 99, 15, 13, 12, 1]
+
+console.log(numbers.sort(function(a, b){
+    return a ===b ?0: a>b?1:-1;
+})); //  [1, 12, 13, 15, 99, 1000]
+
+// Splice Method
+
+//Synatx
+// splice(start, deletecount, item, item1,...)
+
+const names3 = ['tom', 'alex', 'bob'];
+
+console.log(names3.splice(0,1,"Jack")); // ['tom']
+
+console.log(names3); // ['Jack', 'alex', 'bob']
+
+//EX2:
+const names4 = ['tom', 'alex', 'bob'];
+
+console.log(names4.splice(1,0,"Jack")); // []
+
+console.log(names4); //  ['tom', 'Jack', 'alex', 'bob']
+
+//EX3:
+const names5 = ['tom', 'alex', 'bob'];
+
+console.log(names5.splice(0,4,"Jack")); // ['tom', 'alex', 'bob']
+
+console.log(names5); // ['Jack']
+
+// At method
+//fetch the data based on index both postive and negtive
+
+const junkfood = ['hotdog', 'burger', 'fries','pizza', 'popcorn','sandwitch'];
+
+console.log(junkfood.at(0)); // hotdog
+console.log(junkfood.at(3)); // pizza
+console.log(junkfood.at(5)); // sandwitch
+console.log(junkfood.at(6)); // undefined
+console.log(junkfood.at(-1)); // sandwitch
+console.log(junkfood.at(-4)); // fries
+
+// Copy with in method
+
+// synatx
+// copywithin(target, start, end)
+
+const array = [1,2,3,4,5,6,7];
+array.copyWithin(0,3,8);
+console.log(array); // [4, 5, 6, 4, 5, 6, 7]
+
+const array1 = [1,2,3,4,5,6,7,8];
+array1.copyWithin(2,3,9);
+console.log(array1); // [1, 2, 4, 5, 6, 7, 8, 8]
+
+const array2 = [1,2,3,4,5,6,7,8];
+array2.copyWithin(2,3);
+console.log(array2); // [1, 2, 4, 5, 6, 7, 8, 8]
+
+//Flat Method
+// Convert to nested array to flat array
+
+const arr3 = [0,1,2,[3,4]];
+console.log(arr3.flat()); // [0, 1, 2, 3, 4]
+
+const arr4 = [0,1,2,[3,[4,5,[6]]]];
+console.log(arr4.flat()); // [0, 1, 2, 3, Array(3)]
+
+const arr5 = [0,1,2,[3,[4,5,[6]]]];
+console.log(arr5.flat(2)); // [0, 1, 2, 3, 4, 5, Array(1)]
+
+const arr6 = [0,1,2,[3,[4,5,[6]]]];
+console.log(arr6.flat(Infinity)); // [0, 1, 2, 3, 4, 5, 6]
