a27c73634530a6bf7dc82d93729dec783819216f
ShrikiQA <shriki1019@gmail.com>
2026-03-30 22:23:56 +0530

    Day 15 Task-1

--- day-15/task15.js
+++ day-15/task15.js
@@ -1,3 +1,5 @@
+//04  Use the for loop on the above array to print elements in the odd index.
+
 console.log("Welcome to task for day 15");
 
 const salad = ["tomato", "mushroom", "brolie","carrot"];
@@ -9,3 +11,26 @@ for(let i=0;i<salad.length;i++)
         console.log(salad[i]);
     }
 }
+
+//01 Create an array of 5 elements using the Array Constructor.
+
+const arr1 = new Array(1,2,3,4,5);
+console.log(arr1);
+
+// 02 Create an array of 3 empty slots.
+
+const arr2 = [ , , , ];
+console.log(arr2);
+
+//03 Create an array of 6 elements using the Array literals and access the fourth element in the array using its length property.
+
+const arr3 = ["A", "B", "C", "D", "E", "F"];
+
+console.log(arr3[arr3.length-3]); // D
+
+// 05 Add one element at the front and the end of an array.
+
+const arr5 = ["B", "C", "D"];
+arr5.push("E");// End
+arr5.unshift("A"); // Front
+console.log(arr5); // ['A', 'B', 'C', 'D', 'E']
